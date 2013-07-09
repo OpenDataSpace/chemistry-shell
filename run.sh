625133b36675504da7b54160032c16f0f6a9cc15
Jenkins Continuous Build server <jenkins@dataspace.cc>
2013-07-09 16:42:35 +0200

    Releasing new candidate 0.5.22

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 BASEDIR=`dirname $0`
 #JAVA_OPTS="$JAVA_OPTS -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java $JAVA_OPTS -jar "$BASEDIR"/target/chemistry-shell-0.5.21.jar "$@"
+java $JAVA_OPTS -jar "$BASEDIR"/target/chemistry-shell-0.5.22.jar "$@"
