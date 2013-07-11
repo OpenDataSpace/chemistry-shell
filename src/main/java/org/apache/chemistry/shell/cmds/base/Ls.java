c309745911d417f3a23609ae3e5076c0f4ae6067
Till Lorentzen <till.lorentzen@graudata.com>
2013-07-11 21:25:54 +0200

    Started to implement ls -l option

--- src/main/java/org/apache/chemistry/shell/cmds/base/Ls.java
+++ src/main/java/org/apache/chemistry/shell/cmds/base/Ls.java
@@ -24,6 +24,8 @@
 
 package org.apache.chemistry.shell.cmds.base;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.chemistry.opencmis.client.api.Document;
 import org.apache.chemistry.shell.app.Application;
 import org.apache.chemistry.shell.app.Context;
@@ -31,38 +33,63 @@ import org.apache.chemistry.shell.command.Cmd;
 import org.apache.chemistry.shell.command.Command;
 import org.apache.chemistry.shell.command.CommandException;
 import org.apache.chemistry.shell.command.CommandLine;
+import org.apache.chemistry.shell.command.CommandParameter;
 import org.apache.chemistry.shell.util.Path;
 
-@Cmd(syntax = "ls [target:item]", synopsis = "List entries in working directory")
+@Cmd(syntax = "ls [-l] [target:item]", synopsis = "List entries in working directory")
 public class Ls extends Command {
 
-    @Override
-    public void run(Application app, CommandLine cmdLine) throws Exception {
-        ensureConnected(app);
+	@Override
+	public void run(Application app, CommandLine cmdLine) throws Exception {
+		ensureConnected(app);
 
-        String param = cmdLine.getParameterValue("target");
-
-        Context ctx;
-        if (param == null) {
-            ctx = app.getContext();
-            ctx.reset();
-            for (String line : ctx.ls()) {
-                println(line);
-            }
-        } else {
-            ctx = app.resolveContext(new Path(param));
-            if (ctx == null) {
-                throw new CommandException("Cannot resolve target: " + param);
-            }
-            Document document = ctx.as(Document.class);
-            if (document != null) {
-                println(document.getName());
-            } else {
-                for (String line : ctx.ls()) {
-                    println(line);
-                }
-            }
-        }
-    }
-
+		String param = cmdLine.getParameterValue("target");
+		CommandParameter lParam = cmdLine.getParameter("-l");
+		Context ctx;
+		if (param == null) {
+			ctx = app.getContext();
+			ctx.reset();
+			for (String line : ctx.ls()) {
+				println(line);
+			}
+		} else {
+			ctx = app.resolveContext(new Path(param));
+			if (ctx == null) {
+				throw new CommandException("Cannot resolve target: " + param);
+			}
+			Document document = ctx.as(Document.class);
+			// TODO interpret -l arguments correctly
+			if (document != null) {
+				if (lParam != null) {
+					String acls = "?????????";
+					if (document.getAcl() != null) {
+						acls += "+";
+					} else {
+						if (document.getProperty("cmis:isImmutable").equals(
+								Boolean.TRUE)) {
+							acls = "?-??-??-?";
+						} else {
+							acls = "?????????";
+						}
+					}
+					int hardLinks = document.getParents().size();
+					String owner = document.getLastModifiedBy();
+					String group = document.getCreatedBy();
+					long size = document.getContentStreamLength();
+					String modDate = new SimpleDateFormat("yyyy MMM dd hh:mm")
+							.format(document.getLastModificationDate()
+									.getTime());
+					String name = document.getName();
+					println(String.format("-%s %d %s %s %d %s %s", acls,
+							hardLinks, owner, group, size, modDate, name));
+				} else {
+					println(document.getName());
+				}
+			} else {
+				for (String line : ctx.ls()) {
+					println(line);
+				}
+			}
+		}
+	}
 }
